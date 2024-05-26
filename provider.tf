b396818d5e88e4bbae92ce6d5503bcb3f635e40d
Orlando Gentil <orlando.gentil@gmail.com>
2024-05-26 22:09:12 +0000

    first configuration

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+    }
+  }
+}
+provider "oci" {
+  # tenancy_ocid = "<tenancy OCID>"
+  # user_ocid = " ocid1.compartment.oc1..aaaa"
+  # private_key_path = ”~/keys/priv.pem"
+  # fingerprint = "AA:11:BB:22"
+  region = var.region
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "region" {}
+variable "compartment_ocid" {}
+variable "vcn01_cidr_block" {}
+variable "vcn01_display_name" {}
